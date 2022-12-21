73d3afdf9f57c3efc05550e8efdc901736c70728
madanaa <madandev1994@gmail.com>
2022-12-22 02:16:59 +0530

    added

--- src/day7/GCD.java
+++ src/day7/GCD.java
@@ -1,2 +1,17 @@
-package day7;public class GCD {
+package day7;
+
+public class GCD {
+    public static int gcd(int a, int b){
+        int gcdd = 0;
+        for (int i = 1; i <= a && i <= b;i++) {
+            if (a % i == 0 && b % i ==0) {
+                gcdd = i;
+            }
+        }
+        return gcdd;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcd(15,25));
+    }
 }
