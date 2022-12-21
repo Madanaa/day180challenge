c43d672f5bfc4745aba5c57029b50bbba10eca80
madanaa <madandev1994@gmail.com>
2022-12-22 02:05:14 +0530

    added

--- src/day7/PowerOfA.java
+++ src/day7/PowerOfA.java
@@ -1,2 +1,15 @@
-package day7;public class PowerOfA {
+package day7;
+
+public class PowerOfA {
+    public static void main(String[] args) {
+        int n = 4;
+        int exponent = 2;
+        int p = 1;
+        int i = 1;
+        while (exponent >= i ) {
+            p = p * n;
+            i++;
+        }
+        System.out.println(p);
+    }
 }
