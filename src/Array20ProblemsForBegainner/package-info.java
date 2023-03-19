48da5a3f3627204985739de96d25ca7fc6b6f832
madanaa <madandev1994@gmail.com>
2023-03-19 17:41:43 +0530

    added

---
+++ src/Array20ProblemsForBegainner/package-info.java
@@ -0,0 +1,26 @@
+package Array20ProblemsForBegainner;
+/*
+here are 20 array-based questions with varying difficulty levels:
+
+1.Write a program to print the elements of an array in reverse order.
+2.Given an array of integers, write a program to find the sum of all the even numbers in the array.
+3.Write a program to find the largest element in an array.
+4.Given an array of integers, write a program to find the sum of all the elements in the array.
+5.Write a program to sort an array of integers in ascending order.
+6.Given an array of integers, write a program to find the median of the array.
+7.Write a program to remove all the negative elements from an array.
+8.Given an array of integers, write a program to find the product of all the elements in the array.
+9.Write a program to reverse the order of elements in an array in groups of k.
+10.Given two arrays of equal length, write a program to find the dot product of the arrays.
+11.Write a program to find the smallest and largest elements in an unsorted array.
+12.Given an array of integers, write a program to find the second largest element in the array.
+13.Write a program to find the kth smallest element in an unsorted array.
+14.Given an array of integers, write a program to find the elements that appear more than once in the array.
+15.Write a program to find the elements that appear exactly once in an array.
+16.Given two sorted arrays, write a program to merge them into a single sorted array.
+17.Write a program to remove duplicates from an array.
+18.Given an array of integers, write a program to find the length of the longest increasing subsequence.
+19.Write a program to find the pair of elements in an array whose sum is closest to zero.
+20.Given an array of integers, write a program to find the contiguous subarray with the largest sum.
+These questions cover a variety of array-related topics and can be useful for interview preparation or practicing coding skills.
+ */
