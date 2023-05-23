8512bb27d85d7695e9bf0107e7d334b074228681
madanaa <madandev1994@gmail.com>
2023-05-24 03:34:20 +0530

    added

--- src/array/PairSum.java
+++ src/array/PairSum.java
@@ -1,5 +1,22 @@
 package array;
 
 public class PairSum {
+    // this brute force approach
+    public static int pairSum(int a[], int k) {
+        int count = 0;
+        for (int i = 0; i < a.length;i++) {
+            for (int j = i + 1; j < a.length;j++) {
+                if (Math.abs(a[i] - a[j]) == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 
+    public static void main(String[] args) {
+        int a[] = {1,2,3,4};
+        int k = 1;
+        System.out.println(pairSum(a,k));
+    }
 }
