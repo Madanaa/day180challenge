344d71ff348ce6237c2ab0f4197be4c9faabe59b
madanaa <madandev1994@gmail.com>
2023-05-28 23:57:42 +0530

    added

--- src/day24/ClassAndObject.java
+++ src/day24/ClassAndObject.java
@@ -1,10 +1,13 @@
 package day24;
 
+
 public class ClassAndObject {
     public static void main(String[] args) {
         Animal animal = new Animal();
         animal.fun();
         Dog dog = new Dog();
         dog.fun();
+        Ss1 s = new Ss1();
+
     }
 }
---
+++ src/day24/Ss1.java
@@ -0,0 +1,11 @@
+package day24;
+
+import day24.Dog;
+
+public class Ss1 extends Dog {
+    int ss1 = 147;
+    int ss2 = 254;
+   void foo() {
+       System.out.println("Hello");
+   }
+}
