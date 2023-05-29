f60427a1179ff1d6bc977a4578781c6c981f0e54
madanaa <madandev1994@gmail.com>
2023-05-29 22:54:05 +0530

    added

---
+++ src/array/InsertionSort.java
@@ -0,0 +1,12 @@
+package array;
+
+public class InsertionSort {
+      public static int inserionSort(int a[]) {
+          for (int i = 1; i < a.length; i++) {
+
+          }
+      }
+    public static void main(String[] args) {
+
+    }
+}
