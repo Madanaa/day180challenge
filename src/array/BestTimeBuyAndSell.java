ae3d3485ff74e864482d9ac663d3ce7dac82e151
madanaa <madandev1994@gmail.com>
2023-07-05 00:50:04 +0530

    added

---
+++ src/array/BestTimeBuyAndSell.java
@@ -0,0 +1,18 @@
+package array;
+
+public class BestTimeBuyAndSell {
+    public static int maxProfitBuySell(int price[]) {
+        int maxProfit = 0;
+        int n = price.length;
+        for (int i = 1; i < n; i++) {
+            if (price[i] > price[i-1]) {
+                maxProfit = maxProfit + (price[i] - price[i-1]);
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int price[] = {7,1,5,3,6,4};
+        System.out.println(maxProfitBuySell(price));
+    }
+}
